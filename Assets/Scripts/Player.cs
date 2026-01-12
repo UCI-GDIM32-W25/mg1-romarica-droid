05fade67e4ad3dd4c7827ed95fba29a5f91f6c31
romarica-droid <romarica@uci.edu>
2026-01-11 19:40:22 -0800

    Me when i finish the game by accident?

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,44 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
-    }
+        if(Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
+
+    }
     public void PlantSeed ()
     {
-        
+        if(_numSeedsLeft > 0)
+        {
+            Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+            _numSeedsLeft--;
+            _numSeedsPlanted++;
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+        }
     }
 }
